f79456eec009dbcc9d36e307baa0840f458b5a92
Mark Warneke <mark@Marks-MacBook-Pro.local>
2019-09-17 23:18:16 +0200

    make a funciton out of it

--- New-Solution.ps1
+++ New-Solution.ps1
@@ -1,3 +1,14 @@
+<#
+    .SYNOPSIS
+    Creates a new class lib based on the solution name
+    
+    .PARAMETER SolutionName
+    Name of the new solution to be created
+    
+    .EXAMPLE
+    ./New-Solution.ps1 -SolutionName "Solution"
+
+#>
 param(
     # Solution Name
     [Parameter(mandatory)]
@@ -5,23 +16,54 @@ param(
     $SolutionName
 )
 
-$SolutionTestName = "$SolutionName.Tests"
+function New-Solution {
+    <#
+        .SYNOPSIS
+        Creates a new class lib based on the solution name
+        
+        .DESCRIPTION
+        Creates a new directory called Soltuion Name,
+        initializes a new class lib based on the solution name
+        Adds the the new lib to the solution
+        Creates a new directory for tests based on solution name
+        Initializes new xunit project
+        Adds reference to previous created solution
+        Adds the new xunit project to solution
+        
+        .PARAMETER SolutionName
+        Name of the new solution to be created
+        
+        .EXAMPLE
+        ./New-Solution.ps1 -SolutionName "Solution"
+    #>
+    
+    param(
+        # Solution Name
+        [Parameter(mandatory)]
+        [string]
+        $SolutionName
+    )
 
-# Create Solution
-New-Item -Name  "$SolutionName" -Type Directory
-Push-Location $SolutionName
-dotnet new classlib
+    $SolutionTestName = "$SolutionName.Tests"
 
-# Add to sln file
-Pop-Location
-dotnet sln add ./$SolutionName/$SolutionName.csproj
+    # Create Solution
+    New-Item -Name  "$SolutionName" -Type Directory
+    Push-Location $SolutionName
+    dotnet new classlib
 
-# Add Test for Solution
-New-Item -Name "$SolutioNTestName" -Type Directory
-Push-Location $SolutioNTestName
-dotnet new xunit
-dotnet add reference ../$Solution/$Solution.csproj
+    # Add to sln file
+    Pop-Location
+    dotnet sln add ./$SolutionName/$SolutionName.csproj
 
-# Add to sln file
-Pop-Location
-dotnet sln add ./$SolutionTestName/$SolutionTestName.csproj
+    # Add Test for Solution
+    New-Item -Name "$SolutioNTestName" -Type Directory
+    Push-Location $SolutioNTestName
+    dotnet new xunit
+    dotnet add reference ../$Solution/$Solution.csproj
+
+    # Add to sln file
+    Pop-Location
+    dotnet sln add ./$SolutionTestName/$SolutionTestName.csproj
+}
+
+New-Solution $SolutionName
